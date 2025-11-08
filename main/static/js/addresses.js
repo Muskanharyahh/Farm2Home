a34462bd2caaf04629c6c1e031f97ac4d56b8a44
Eman Saleem <saleem4630046@cloud.neduet.edu.pk>
2025-11-09 00:19:56 +0500

    finally server started

--- main/static/js/addresses.js
+++
@@ -1,6 +0,0 @@
-// Addresses page JavaScript
-console.log('Addresses JS loaded');
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Add addresses functionality here
-});
--- main/static/js/orders.js
+++
@@ -1,6 +0,0 @@
-// Orders page JavaScript
-console.log('Orders JS loaded');
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Add orders functionality here
-});
